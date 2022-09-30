679ed49adf1ba678ece7354ce6a67eb44be79bd6
Melkor <rintfd@163.com>
2022-09-30 15:10:46 +0800

    Update utils.py

--- euclidean_learner/utils.py
+++ euclidean_learner/utils.py
@@ -28,7 +28,7 @@ def parse_concept_program(program = ["l1 = line(p1(), p2())"]):
     """
     ground_elements = []
     for line in program:
-        left,right = line.split("=")
+        left,right = line.replace(" ","").split("=")
         ground_elements.append(left)
     return ground_elements
 
