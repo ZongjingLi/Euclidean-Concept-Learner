d3c89d8b6fe71aed066788e2f58eb546fbc975bd
Melkor <rintfd@163.com>
2022-09-30 08:14:19 +0800

    Update utils.py

--- euclidean_learner/utils.py
+++ euclidean_learner/utils.py
@@ -6,3 +6,8 @@ v1 = Variable(torch.randn([1,32]),requires_grad = True)
 def make_grid(resolution = (128,128)):
     # make the grid with the shape of [w,h]
     return resolution
+
+def make_points(num = 1,distribution = "normal"):
+    return [Variable(torch.randn(2),requires_grad = True) for _ in range(num)]
+
+print(make_points(3))
