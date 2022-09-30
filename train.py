0d48142062064aacab0d92d3a501700b051c4945
Melkor <rintfd@163.com>
2022-09-30 08:07:43 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,10 @@
 from euclidean_learner import *
 
 if __name__ == "__main__":
-    pass
+    concept_dataset = EuclidData("train",name = "ccc")
+    print(len(concept_dataset))
+    
+    dataloader = DataLoader(concept_dataset,batch_size = 1)
+    
+    for sample in dataloader:
+        print(sample)
