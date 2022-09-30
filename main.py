76da433e65a461af90f608a036c9b6bcb2158cfa
Melkor <rintfd@163.com>
2022-09-30 13:47:34 +0800

    update

--- main.py
+++ main.py
@@ -17,24 +17,30 @@ grid = make_grid().permute([1,2,0])
 
 grid_expand = grid.flatten(start_dim = 0, end_dim = 1).unsqueeze(0).repeat([100,1,1])
 
-
+"""
+def harmard_min(source,set):
+    outputs = []
+    for i in range(source.size(0)):
+        outputs.append(torch.min(source - set,-1).values())
+    return torch.stack(outputs)
+"""
 
 diff = grid_expand - test_line.unsqueeze(1).repeat([1,16384,1])
 print(diff.shape)
-min_diff = torch.min(diff,dim = 0).values
-
-print(min_diff.shape)
-
-leng_diff = torch.sum(torch.abs(min_diff),-1)
+leng_diff = torch.norm(diff,dim = -1)
 
 print(leng_diff.shape)
 
-plt.plot(leng_diff)
+min_diff = torch.min(leng_diff,0).values
+
+print(min_diff.shape)
+
+plt.plot(min_diff)
 plt.show()
 
 line_norm = dists.Normal(0,1)
 
-logpdf = line_norm.log_prob(leng_diff)
+logpdf = line_norm.log_prob(min_diff)
 
 
 logpdf = logpdf.view([128,128,1])
