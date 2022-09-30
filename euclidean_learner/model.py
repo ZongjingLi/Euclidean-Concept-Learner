6b49b16013515aefeda3b62fb4c369797f88f496
Melkor <rintfd@163.com>
2022-09-30 08:18:21 +0800

    Update model.py

--- euclidean_learner/model.py
+++ euclidean_learner/model.py
@@ -13,3 +13,20 @@ class ConceptModelSearch(nn.Module):
 
     def forward(self,x):
         return x
+
+class EuclidConceptModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        """
+        An euclid concept program is either a:
+        point (x,y)  line(p1,p2) circle(p1,p2)
+        or the composition of these things (constraint, and)
+        """
+        self.euclid_program = None
+
+    def forward(self,x,format = "logp"):
+        """
+        logp          : return the log probability of image x is created by the model
+        cross_entropy : return the cross entropy of the image x created by the model
+        """
+        return 0
