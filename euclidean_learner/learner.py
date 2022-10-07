da0c9622bfe64667ae083b3cac9f92e7dd5a47fa
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-07 08:16:08 +0800

    Update learner.py

--- euclidean_learner/learner.py
+++ euclidean_learner/learner.py
@@ -1,2 +1,13 @@
 from encoder import * # import all the encoder models
 from model   import * # import all the concept models
+
+from config import * # load the geometric model config
+
+class GeometricLearner(nn.Module):
+    def __init__(self,model_opt):
+        super().__init__()
+        self.encoder = GeometricEncoder()
+        self.concept = None
+
+    def forward(self,x):
+        return x
