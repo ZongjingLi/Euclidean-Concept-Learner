1e4ff194dd6a7ef33a55e2b1ce4e58674b71d75b
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-07 12:40:02 +0800

    Update model.py

--- geometric_learner/model.py
+++ geometric_learner/model.py
@@ -11,11 +11,21 @@ class GeometricAutoEncoder(nn.Module):
         super().__init__()
         self.encoder = GeometricEncoder(3,132)
         self.asearch = None
+        self.structure = None
 
     def find_concept_struct(self,image):
         return 0
 
     def forward(self,image,concept_struct = None):
+        """
+        inoput concept struct should be a list of func-node programs
+        """
+        if isinstance(concept_struct,str):print("concept struct is an instance")
         if concept_struct is None:concept_struct = self.find_concept_struct(image)
-        encoder_features = self.encoder(image)
+        encoder_features = self.encoder(image) # encoder the global prior feature
+
+        self.structure.make_dag(concept_struct)
+        self.structure.realize(encoder_features) # use the 
         return image
+
+    def namomo(self):return "namomo"
