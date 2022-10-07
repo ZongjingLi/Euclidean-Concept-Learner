82c69db22ccfac4211db55a7cd3f29827a4ca34d
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-07 09:45:00 +0800

    update the geometric learner

--- euclidean_learner/learner.py
+++ euclidean_learner/learner.py
@@ -2,12 +2,18 @@ from encoder import * # import all the encoder models
 from model   import * # import all the concept models
 
 from config import * # load the geometric model config
-
+"""
+This Geometric Learner is practically a Auto-Encoder given a fixed concept structure
+    (encoder): geometric encoder that encoder the global information
+    (decoder): the concept structure created by the 
+"""
 class GeometricLearner(nn.Module):
     def __init__(self,model_opt):
         super().__init__()
-        self.encoder = GeometricEncoder()
+        self.encoder = GeometricEncoder(3,64)
         self.concept = None
 
     def forward(self,x):
-        return x
+        feature_map = self.encoder(x)
+        print(feature_map.shape)
+        return feature_map
