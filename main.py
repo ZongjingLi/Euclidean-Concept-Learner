1b8a5754ec294b70e21637ac394990b46e52b31b
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-07 23:49:14 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 
 from geometric_learner.model import *
+from geometric_learner.primitive import *
 
 dataset = EuclidConceptData("train","angle")
 loader = DataLoader(dataset,batch_size = 1)
+struct = GeometricStructure()
 
 for sample in loader:
-    print(sample["programs"])
+    for t in sample["programs"]:print(t)
+    programs = [term[0] for term in sample["programs"]]
+    struct.make_dag(programs)
+    print("")
