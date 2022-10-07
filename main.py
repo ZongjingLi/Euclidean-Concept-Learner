b4255f680e02f10c8dcbe0dbfa65f3c76290a663
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-10-07 23:36:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from geometric_learner.model import *
 
 dataset = EuclidConceptData("train","angle")
+loader = DataLoader(dataset,batch_size = 1)
 
-for sample in dataset:
+for sample in loader:
     print(sample["programs"])
